59c0cde652344eb4d527bab2d46f42f404408897
Henri Remonen <henri@remonen.fi>
2022-11-08 13:11:28 +0200

    Osa 4 tehtävä 4

--- viikko 4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko 4/verkkokauppa/src/tests/kauppa_test.py
@@ -112,3 +112,51 @@ class TestKauppa(unittest.TestCase):
             "12345",
             "33333-44455",
             5)
+
+    def test_aloita_asiointi_nollaa_edellisen_asioinnin(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 
+            42,
+            "12345",
+            "33333-44455",
+            1)
+
+    def test_uusi_viitenumero_haettu_jokaisella_maksukerralla(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.tilimaksu("rekka", "67890")
+
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
+
+    def test_ostoskorista_poistettu_tuote_kori_päivittyy(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.poista_korista(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.pankki_mock.tilisiirto.assert_called_with(
+            "pekka", 
+            42,
+            "12345",
+            "33333-44455",
+            0)
+        
+    def test_poistettu_tuote_palautuu_varastoon(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.poista_korista(1)
+
+        self.varasto_mock.palauta_varastoon.assert_called()
