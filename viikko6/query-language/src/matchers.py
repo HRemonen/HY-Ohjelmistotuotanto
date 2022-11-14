25e56e7b815f5201428239cb28273d26b63f1646
Henri Remonen <henri@remonen.fi>
2022-11-14 18:52:56 +0200

    Add Not class to matchers

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -9,6 +9,21 @@ class And:
         
         return True
 
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+    
+    def matches(self, player):
+        for matcher in self._matchers:
+            if not matcher.matches(player):
+                return True
+        
+        return False
+
+class HasFewerThan:
+    def __init__(self) -> None:
+        pass
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
